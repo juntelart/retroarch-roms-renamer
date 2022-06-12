c9888385ed4fc58abf9a5c6e5a8aa531e6611be4
juntelart <klass.juntelart@gmail.com>
2022-06-12 16:09:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Retroarch ROMs Renamer
+
+It's a little script to rename roms according to libretro names convenion for that the thmbnails download feature match with correct thumbnail
+
+## STEPS
+
+### Clone this repository
+
+```bash
+git clone https://github.com/juntelart/retroarch-roms-renamer.git
+cd retroarch-roms-renamer
+```
+
+### Check libretro thumbnails URL
+Go to http://thumbnails.libretro.com/ Just check what is a libretro thumbnails url (You can use Boxarts folder or Snaps or Titles), for example for NES:
+
+http://thumbnails.libretro.com/Nintendo%20-%20Nintendo%20Entertainment%20System/Named_Boxarts/
+
+### Execute script
+
+To execute script you shouls to pass 3 params:
+* Libretro thumbnails URL (http://thumbnails.libretro.com/[system]/Named_Boxarts/)
+* Local ROMs path (E.g. /media/user/roms/Nintendo Entertainment System)
+* ROMs extension (E.g. .nes)
+  
+Example (for NES):
+```bash
+python3 rrr.py "http://thumbnails.libretro.com/Nintendo%20-%20Nintendo%20Entertainment%20System/Named_Boxarts/" "/media/user/roms/Nintendo Entertainment System" ".nes"
---
+++ rrr.py
@@ -0,0 +1,68 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+import glob
+import os
+import re
+
+params_count = len(sys.argv)
+
+if params_count != 4:
+    print("Error: Missing parameters");
+    print("Usage: python3 " + sys.argv[0] + " \"http://url-to-thumbnails\" \"/local/path/to/roms\" \".ext\"")
+    print("E.g. python3 " + sys.argv[0] + " \"http://thumbnails.libretro.com/Nintendo%20-%20Nintendo%20Entertainment%20System/Named_Boxarts/\" \"/media/r/USB/roms/Nintendo Entertainment System\" \".nes\"")
+    sys.exit()
+
+url = sys.argv[1]
+path = sys.argv[2]
+rom_extension = sys.argv[3]
+
+if not os.path.isdir(path):
+    print("Local path doesn't exist")
+    print("Usage: python3 " + sys.argv[0] + " \"http://url-to-thumbnails\" \"/local/path/to/roms\" \".ext\"")
+    print("E.g. python3 " + sys.argv[0] + " \"http://thumbnails.libretro.com/Nintendo%20-%20Nintendo%20Entertainment%20System/Named_Boxarts/\" \"/media/r/USB/roms/Nintendo Entertainment System\" \".nes\"")
+    sys.exit()
+
+try:
+    print("Retrieving libretro names...")
+    response = requests.get(url)
+    if not response.ok:
+        print("Error connectirng to thumbails url")
+        sys.exit()
+    
+    html = BeautifulSoup(response.text, 'html.parser')
+    links = html.find_all('a')
+    libretroNames = dict()
+    for link in links:
+        name = unquote(link['href'])
+        name_without_extension = name.split(".png")[0]
+        key = name_without_extension.split("(")[0]
+        libretroNames[key] = name_without_extension
+except requests.exceptions.ConnectionError as exc:
+    print("Error connectirng to thumbails url")
+    sys.exit()
+
+os.chdir(path)
+
+print("Renaming files...")
+renamed_files_count = 0
+for file in glob.glob("*" + rom_extension):
+    file_without_extension = file.split(rom_extension)[0]
+    forbidden_characters = r'[&*/:`<>?\|]'
+    clean_file_name = re.sub(forbidden_characters, '_', file_without_extension)
+
+    if (clean_file_name != file_without_extension):
+        print("Forbidden character detected. Renaming...")
+        os.rename(file_without_extension + rom_extension, clean_file_name + rom_extension)    
+
+    key = file_without_extension.split("(")[0]
+    if key in libretroNames:
+        rightName = libretroNames[key] + rom_extension
+        if (file != rightName):
+            print(file + " -> " + rightName)
+            renamed_files_count+=1
+            os.rename(clean_file_name + rom_extension, rightName)
+
+print("Renamed " + str(renamed_files_count) + " files")
+
